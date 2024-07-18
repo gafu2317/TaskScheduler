1f5f00301ac7d97c62842da10b118dd3e025688d
gafu2317 <0002317rynkmk@gmail.com>
2024-07-19 03:46:37 +0900

    update remove not necessary files

--- task-scheduler/src/Components/Scheduler/Scheduler.tsx
+++ task-scheduler/src/Components/Scheduler/Scheduler.tsx
@@ -1,37 +1,26 @@
-import React from "react";
+import { useDroppable } from "@dnd-kit/core";
+import { FC, ReactNode } from "react";
 
-const scheduler = () => {
+type DroppableProp = {
+  children: ReactNode;
+  id: string;
+};
+
+const Scheduler: FC<DroppableProp> = ({ children, id }) => {
+  // const { setNodeRef, isOver } = useDroppable({
+  //   id,
+  // });
+
   return (
-    <div>
-      <h2>24時間スケジュール</h2>
-      <div>
-        {/* <ul>1:00</ul>
-        <ul>2:00</ul>
-        <ul>3:00</ul>
-        <ul>4:00</ul>
-        <ul>5:00</ul>
-        <ul>6:00</ul>
-        <ul>7:00</ul>
-        <ul>8:00</ul>
-        <ul>9:00</ul>
-        <ul>10:00</ul>
-        <ul>11:00</ul>
-        <ul>12:00</ul>
-        <ul>13:00</ul>
-        <ul>14:00</ul>
-        <ul>15:00</ul>
-        <ul>16:00</ul>
-        <ul>17:00</ul>
-        <ul>18:00</ul>
-        <ul>19:00</ul>
-        <ul>20:00</ul>
-        <ul>21:00</ul>
-        <ul>22:00</ul>
-        <ul>23:00</ul>
-        <ul>24:00</ul> */}
-      </div>
+    <div
+      // ref={setNodeRef}
+      // style={{
+      //   backgroundColor: isOver ? "lightblue" : "undifined",
+      // }}
+    >
+      {children}
     </div>
   );
 };
 
-export default scheduler;
+export default Scheduler;
--- task-scheduler/src/Components/Scheduler/SchedulerDay.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const schedulerDay = () => {
-  return (
-    <div>schedulerDay</div>
-  )
-}
-
-export default schedulerDay
